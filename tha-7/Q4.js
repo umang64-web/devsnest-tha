99d08154467804d4c6d3c0328f9fcd0d13ccfad3
umang64-web <umangaggarwal64@gmail.com>
2021-06-15 01:48:57 +0530

    tha7

---
+++ tha-7/Q4.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
+var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
+    for(var i=0;i<library.length;i++){
+        var book="'"+library[i].title+ "'"+' by '+library[i].author;
+        if(library[i].readingStatus)
+        {
+            console.log('already read '+book);
+        }
+        else
+        console.log('still have to read '+book)
+}
---
+++ tha-7/Q5.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes. Volume of a cylinder : V = πr2h where r is the radius and h is the height of the cylinder. Try To Attempt : Difficult Level Increased 
+
+function Cylinder(cyl_height, cyl_diameter) {
+    this.cyl_height = cyl_height;
+    this.cyl_diameter = cyl_diameter;
+  }
+  
+  Cylinder.prototype.Volume = function () {
+    var radius = this.cyl_diameter / 2;
+    return this.cyl_height * Math.PI * radius * radius;
+  };
+  
+  var cyl = new Cylinder(80, 4);
+  // Volume of the cylinder with four decimal places.
+  console.log('volume =', cyl.Volume().toFixed(4));
+  
