4bedaa9e2f5008cf248739da469dd2eb9f995136
umang64-web <83840246+umang64-web@users.noreply.github.com>
2021-09-15 18:14:49 +0530

    Delete Day-26 directory

--- Day-26/redux/store.js
+++
@@ -1,5 +0,0 @@
-import {createStore } from "redux";
-import {userFormReducer} from "./userForm/userFormReducer"
-const store = createStore(userFormReducer);
-
-export default store;
--- Day-26/redux/userForm/userFormActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const updateFirstName = "updateFirstName";
-export const updateLastName = "updateLastName";
